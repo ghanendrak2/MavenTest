e7922bee920ae7739ae74516e2ad32fa8999b141
Ghanendrak2 <ghanendrak2@gmail.com>
2018-06-01 17:25:47 +0530

    pushing complete code

---
+++ PI/src/main/java/SmartTool/PI/App.java
@@ -0,0 +1,13 @@
+package SmartTool.PI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
